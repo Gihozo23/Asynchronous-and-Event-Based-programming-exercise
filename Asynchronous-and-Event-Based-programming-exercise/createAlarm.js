11df026251736c86488a6690c2f015fcd8f4758f
Gihozo23 <christellegihozo23@gmail.com>
2024-06-21 11:25:38 +0200

    chore: add the createAlarm.js file

--- Asynchronous-and-Event-Based-programming-exercise/createAlarm.js
+++ Asynchronous-and-Event-Based-programming-exercise/createAlarm.js
@@ -1,7 +1,7 @@
-// Create a function called createAlarm that generates a wake-up message for a person after a specified time delay. 
-//This function should accept two parameters: the person's name and the delay time in seconds.The function should 
+// Create a function called createAlarm that generates a wake-up message for a person after a specified time delay.
+//This function should accept two parameters: the person's name and the delay time in seconds.The function should
 //return a promise that resolves with the wake-up message (e.g.Wake up person)
-// but if the delay is less than 2 seconds,the promise should be immediately rejected with an error 
+// but if the delay is less than 2 seconds,the promise should be immediately rejected with an error
 //message stating **Delay is not sufficient**
 
 // **Example of how the function should be used**
@@ -10,30 +10,29 @@
 
 // Copy
 
-
-let createAlarm=(name,delay)=>{
-    return new Promise((resolve,reject)=>{
-    if(delay>=2)
-    {
-        setTimeout(()=>{
-        resolve(`Wake up ${name} after ${delay}`)}, delay*1000)
+let createAlarm = (name, delay) => {
+  return new Promise((resolve, reject) => {
+    if (delay >= 2) {
+      setTimeout(() => {
+        resolve(`Wake up ${name} after ${delay}`);
+      }, delay * 1000);
+    } else {
+      reject("Delay is not sufficient");
     }
-    else{
-        reject("Delay is not sufficient")
-    }}
-)
-}
-createAlarm('John', 4).then((message) => {
-    console.log(message) // output "Wake up John" after 4 seconds
-}).catch((error) => {
-    console.error(error)
-})
+  });
+};
+createAlarm("John", 4)
+  .then((message) => {
+    console.log(message); // output "Wake up John" after 4 seconds
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
-createAlarm('John', 1).then((message) => {
-    console.log(message)
-}).catch((error) => {
-    console.error(error) // output "Delay is not sufficient"
-})
-
-
-
+createAlarm("John", 1)
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((error) => {
+    console.error(error); // output "Delay is not sufficient"
+  });
